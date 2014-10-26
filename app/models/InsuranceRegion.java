bcb2674ce44af92294c0749f34e0032dd4a52b33
Sean Payne <seantpayne@gmail.com>
2014-10-25 19:25:57 -0700

    Corrected package & redefined gender field

--- app/models/InsuranceRegion.java
+++ app/models/InsuranceRegion.java
@@ -1,11 +1,9 @@
-package model;
+package models;
 
 import play.db.ebean.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import java.util.Collection;
 
 /**
  * Created by Sean on 10/25/2014.
@@ -15,11 +13,8 @@ public class InsuranceRegion extends Model {
     @Id
     public long zipBase;
 
-    // Defines the gender:
-    // 0 = male
-    // 1 = female
     @Id
-    public int gender;
+    public String gender;
 
     public double rate0;
     public double rate20;

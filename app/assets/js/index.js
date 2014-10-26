6523a5b4b7a6908fcc9dd03e234a9970c6c9bbad
Sean Payne <seantpayne@gmail.com>
2014-10-25 20:38:00 -0700

    Added column to rates for Zip codes

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -7,7 +7,7 @@ $(function() {
             var appendToTable = function(tableName) {
                 return function(index, element) {
                     $("#"+tableName+" > tbody:last")
-                        .append("<tr><td>"+element.rate0+"</td><td>"+element.rate20+"</td><td>"+element.rate30+"</td><td>"+element.rate40+"</td><td>"+element.rate50+"</td><td>"+element.rate60+"</td><td>"+element.rate70+"</td></tr>");
+                        .append("<tr><td>"+element.zipBase+"</td><td>"+element.rate0+"</td><td>"+element.rate20+"</td><td>"+element.rate30+"</td><td>"+element.rate40+"</td><td>"+element.rate50+"</td><td>"+element.rate60+"</td><td>"+element.rate70+"</td></tr>");
                 };
             };
 

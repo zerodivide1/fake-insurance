1b07a1e8f558bf4ed1caf1a78f2dd7a5cf2898e3
Sean Payne <seantpayne@gmail.com>
2014-10-25 19:47:06 -0700

    Added handler to return all Insurance Rates

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,9 +2,15 @@ package controllers;
 
 import play.*;
 import models.InsuranceRegion;
+import play.data.Form;
+import play.db.ebean.Model;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.List;
+
+import static play.libs.Json.toJson;
 
 public class Application extends Controller {
 
@@ -20,6 +26,11 @@ public class Application extends Controller {
         InsuranceRegion region = Form.form(InsuranceRegion.class).bindFromRequest().get();
         region.save();
         return redirect(routes.Application.addPage());
+    }
+
+    public static Result listRates() {
+        List<InsuranceRegion> rates = new Model.Finder(long.class, InsuranceRegion.class).all();
+        return ok(toJson(rates));
     }
 
 }

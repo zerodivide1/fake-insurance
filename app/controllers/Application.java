2a702725d6c2a235abbae215d75e3798ff8fadd2
Sean Payne <seantpayne@gmail.com>
2014-10-25 19:33:03 -0700

    Added page to add new insurance region

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import models.InsuranceRegion;
 import play.mvc.*;
 
 import views.html.*;
@@ -9,6 +10,16 @@ public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result addPage() {
+        return ok(add.render());
+    }
+
+    public static Result addRegionRate() {
+        InsuranceRegion region = Form.form(InsuranceRegion.class).bindFromRequest().get();
+        region.save();
+        return redirect(routes.Application.addPage());
     }
 
 }
